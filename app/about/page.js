749fb00f9af27fc53ca7d31f50de4721f48fa424
Chayawut <chayawut11@gmail.com>
2024-11-27 16:03:32 +0700

    mynextapp

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1 className="my-6 text-4xl font-bold">About</h1>
+    </div>
+    )
+}
---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+           <h1 className="my-6 text-4xl font-bold">Contact</h1>
+        </div>
+    );
+}
